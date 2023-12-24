127baffb431dc834b035fee9a2e4ae89bef8122d
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-24 16:21:19 +0000

    Added a logo and a div for the 3d animation. Improved styles of tabs.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Tabs from '../templates/tabs';
 
 import '../styles/main.scss';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
 
@@ -23,6 +24,12 @@ const IndexPage = () => {
 
   return (
     <div className='mainDiv'>
+      <div style={{width: 150, height: 150, margin: '40px 0 40px 0'}}>
+          <StaticImage src='../images/icon.png' alt='Gastby logo'/>
+      </div>
+      <div style={{width: '98%', minHeight: 150, backgroundColor: 'gray'}}>
+        A 3D animation goes here
+      </div>
       <Tabs allContent={tabsContent}/>
     </div>
   );
