fab61dfb46aa716ce66a3ea6ce72d085f86db2ed
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-24 14:29:31 +0000

    Created basic gastby app.

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GCCEProject Landing Page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
