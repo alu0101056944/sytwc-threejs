7686f4c7ce601b7d54edba59f12d7c4be66bf728
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-25 22:24:30 +0000

    Default sphere reacts to mouse hover.

--- src/templates/main-animation.js
+++ src/templates/main-animation.js
@@ -18,32 +18,30 @@ function setupScene() {
   const axes = new three.AxesHelper(20);
   scene.add(axes);
 
-  // const sphereGeometry = new three.SphereGeometry(4, 20, 20);
-  // const sphereMaterial = new three.MeshBasicMaterial({
-  //         color:0x7777ff,
-  //         wireframe:true
-  //     });
-  // const sphere = new three.Mesh(sphereGeometry, sphereMaterial);
-  // sphere.position.set(0, 0, 0);
-  // scene.add(sphere);
+  const sphereGeometry = new three.SphereGeometry(4, 20, 20);
+  const sphereMaterial = new three.MeshBasicMaterial({
+          color:0x7777ff,
+          wireframe:true
+      });
+  const sphere = new three.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.set(0, 0, 0);
+  scene.add(sphere);
 
   camera.position.set(10, 30, 5);
   camera.lookAt(scene.position);
 
   const loader = new GLTFLoader();
   loader.load(
-        '/another_test_with_gradient.glb',
+        '/Soldier.glb',
         (gltf) => {
-          const object = gltf.scene.getObjectByName('TorusFinalWithText');
-          scene.add(object);
-          // camera.lookAt(object);
-          // object.rotation.x = 40;
+          scene.add(gltf.scene.children[0]);
         },
         undefined,
         (error) => console.error(error)
       );
 
-  console.log(scene.children);
+  // console.log('our scene: ');
+  // console.log(scene.children);
 
   // clear container first
   const container = document.querySelector('.mainAnimDiv');
@@ -54,7 +52,50 @@ function setupScene() {
   }
   container.appendChild(renderer.domElement);
 
+  updateColorOnMouseHover(scene, camera, renderer);
+
   renderer.render(scene, camera);
+}
+
+function updateColorOnMouseHover(scene, camera, renderer) {
+  const colorPerObjectId = {};
+  let mousePosition = new three.Vector2();
+
+  const raycaster = new three.Raycaster();
+  function changeColorOnMouseOver() {
+    raycaster.setFromCamera(mousePosition, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    for (let i = 0; i < intersects.length; i++) {
+      const object = intersects[i].object;
+      const color = object.material.color;
+      if (color) {
+        colorPerObjectId[object.id] =
+            { r: color.r, g: color.g, b: color.b } ?? null;
+        color.set(0xff0000);
+      }
+    }
+
+    renderer.render(scene, camera);
+
+    // back to original colors after render
+    for (let i = 0; i < intersects.length; i++) {
+      const object = intersects[i].object;
+      const color = object.material.color;
+      if (color) {
+        color.set(colorPerObjectId[object.id].r, colorPerObjectId[object.id].g,
+                colorPerObjectId[object.id].b);
+      }
+    }
+  }
+  
+  window.addEventListener('pointermove', (event) => {
+        const rect = renderer.domElement.getBoundingClientRect();
+        mousePosition.x =
+            ((event.clientX - rect.left) / (rect.right - rect.left)) * 2 - 1;
+        mousePosition.y =
+            - ((event.clientY - rect.top) / (rect.bottom - rect.top)) * 2 + 1;
+        changeColorOnMouseOver();
+      });
 }
 
 const MainAnimation = () => {
