a99effb78545516aa58a025c2acfc5d2bf9f353f
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-27 01:26:24 +0000

    Updates

---
+++ kpi_innerhtml.js
@@ -0,0 +1,20 @@
+export const kpiInnerHTML = `
+<h1>Descargas</h1>
+<a href="/DamagedHelmet.glb">Descargar el modelo de casco.</a> (autor del modelo: KhronosGroup).
+<hr>
+<h1>Métricas</h1>
+
+<ul>
+    <li>Número de retrasos en entregables.</li>
+    <li>El resultado de valoración en encuestas de otros desarrolladores sobre su propio desempeño.</li>
+    <li>Número de cambios drásticos de diseño.</li>
+    <li>Estimación valor de persona-día ideal para la tarjeta.</li>
+    <li>Número de tarjetas completadas en un sprint.</li>
+    <li>Número de retrasos en las tarjetas.</li>
+    <li>Número de quejas del equipo directivo.</li>
+    <li>Número de intervenciones necesarias para que un empleado nuevo se adapte.</li>
+    <li>Número de empleados que van a las sesiones de formación.</li>
+    <li>Tasa de rotación de empleados.</li>
+    <li>Nivel de satisfacción de los empleados desarrolladores.</li>
+</ul>
+`;
---
+++ sytwc-threejs/fetch_innerhtml.js
@@ -0,0 +1,68 @@
+export const fetchInnerHTML = `
+<div>
+  <h1>Descargas</h1>
+  <a href="/sytwc-threejs/fetch_box.glb">Descargar el modelo fetch_box</a>
+  <a href="/sytwc-threejs/animation1_emission.glb">Descargar la escena entera</a>
+  <hr>
+  <h1>Nivel de experiencia de los empleados con la herramienta/tecnología</h1>
+  <ul>
+      <li>n.º de personas que la han utilizado</li>
+      <li>n.º de proyectos que la empresa ha realizado previamente con esa herramienta/tecnología y su nivel de éxito.</li>
+      <li>años de experiencia totales en el equipo.</li>
+      <li>Existencia de otras tecnologías.</li>
+  </ul>
+
+  <h1>Desarrollo tecnológico de las herramientas y librerías</h1>
+  <ul>
+      <li>n.º de cambios de versión/periodo de tiempo</li>
+      <li>número de bugs pendientes por resolver</li>
+      <li>n.º de contribuciones (código abierto)</li>
+      <li>nº de búsquedas a diario</li>
+      <ul>
+          <li>Éxito de otros proyectos con herramientas similares</li>
+          <li>cantidad de proyectos relevantes que lo utilizan y han tenido éxito</li>
+      </ul>
+      <li>Apoyo de comunidad de las herramientas y librerías</li>
+      <ul>
+          <li>n.º y calidad de discusiones en internet</li>
+          <li>nivel de documentación</li>
+          <li>n.º de empresas que utilizan o apoyan la tecnología.</li>
+          <li>n.º de empresas que están buscando trabajadores con experiencia en dicha herramienta.</li>
+      </ul>
+      <li>Costes de licencias</li>
+      <ul>
+          <li>¿Es código abierto?</li>
+          <li>¿Pago periodico o solo una vez?</li>
+      </ul>
+      <li>Decisiones opinionadas realizadas</li>
+      <ul>
+          <li>Ventajas/desventajas de cara al proyecto del cliente?</li>
+      </ul>
+      <li>Facilidad de uso</li>
+      <ul>
+          <li>encuestas al usuario</li>
+          <li>n.º de quejas</li>
+      </ul>
+  </ul>
+
+  <h1>Nivel de satisfacción de clientes</h1>
+  <ul>
+      <li>Encuestas</li>
+      <li>Nivel de éxito del cliente</li>
+  </ul>
+
+  <h1>Niveles de éxito en proyectos que utilizan esas tecnologías</h1>
+  <ul>
+      <li>Número de ventas</li>
+      <li>Beneficio cuatrimestral de la empresa.</li>
+      <li>Número de descargas.</li>
+  </ul>
+
+  <h1>Mercado</h1>
+  <ul>
+      <li>Ofertas de tecnologías</li>
+      <li>N.º de ofertas de trabajo</li>
+      <li>Menciones de tecnologías concretas.</li>
+  </ul>
+<div/>
+`;
